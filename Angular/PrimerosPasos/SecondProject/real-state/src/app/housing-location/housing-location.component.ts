c2fffe9634f874b2d76da7363fcb2e929be70be6
JuanArboledaL <167308178+JuanArboledaL@users.noreply.github.com>
2025-01-30 20:45:31 +0100

    30/01/2025

--- Angular/PrimerosPasos/SecondProject/real-state/src/app/housing-location/housing-location.component.ts
+++ Angular/PrimerosPasos/SecondProject/real-state/src/app/housing-location/housing-location.component.ts
@@ -5,7 +5,14 @@ import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   imports: [],
-  templateUrl: './housing-location.component.html',
+  template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+  </section>
+`,
   styleUrl: './housing-location.component.css'
 })
 
